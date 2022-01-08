90b89cb9e9ece18f7e850933e2aa2b1e3b1ad06d
kjg1993 <kguzman077@gmail.com>
2022-01-08 00:58:04 -0500

    updating code

---
+++ js/main.js
@@ -0,0 +1,31 @@
+"use stric";
+
+count = 1;
+const array = [
+	{
+		label: `week 0${count++}`,
+		url: "https://kjg1993.github.io/wdd330-portfolio/index.html",
+	},
+	{
+		label: `week 0${count++}`,
+		url: "https://kjg1993.github.io/wdd330-portfolio/index.html",
+	},
+];
+
+function table() {
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+
+		const list = document.querySelector(".list");
+		let item = document.createElement("li");
+		let anchor = document.createElement("a");
+		anchor.textContent = element.label;
+		anchor.setAttribute("href", element.url);
+		anchor.setAttribute("target", "_blank");
+
+		item.appendChild(anchor);
+		list.appendChild(item);
+	}
+}
+
+table();
