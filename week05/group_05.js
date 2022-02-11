e038bf5f631b7fb042dc5a389863233784a4df7f
kjg1993 <kguzman077@gmail.com>
2022-02-11 17:11:07 -0600

    Uploading code from week five

---
+++ week05/group_05.js
@@ -0,0 +1,9 @@
+//create an array of hikes
+import Hikes from "./hike-data.js";
+
+const hikeList = new Hikes("hikes");
+
+//on load grab the array and insert it into the page
+window.addEventListener("load", () => {
+	hikeList.showHikeList();
+});
